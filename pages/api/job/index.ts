c2510245cf3bc6986147fc481a521f5be6f30dac
MD Pabel <mdpabel385@gmail.com>
2023-02-26 23:53:36 +0600

    Reset prisma

--- pages/api/job/index.ts
+++ pages/api/job/index.ts
@@ -52,6 +52,8 @@ const handler = nc<ReqType, NextApiResponse>({
         },
       });
 
+      console.log(jobs);
+
       if (!jobs) {
         return res.status(404).json({
           data: 'No jobs found, please post a new job',
