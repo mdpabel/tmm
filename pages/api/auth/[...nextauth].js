b4261c6919e4bb504a1c91511ab56c70a6b6ddef
MD Pabel <mdpabel385@gmail.com>
2023-03-06 16:32:53 +0600

    Fixed session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,98 @@
+// import NextAuth from 'next-auth';
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import prisma from '@/db/postgresql';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+// import { compareHashedPassword } from '@/utils/password';
+// import { jwtVerify, SignJWT } from 'jose';
+// import { createToken } from '@/utils/jwtToken';
+
+// export default NextAuth({
+//   session: {
+//     strategy: 'jwt',
+//     maxAge: 7 * 24 * 60 * 60,
+//   },
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     CredentialsProvider({
+//       name: 'Credentials',
+//       async authorize(credentials, req) {
+//         const { email, password } = credentials;
+
+//         const user = await prisma.user.findUnique({
+//           where: {
+//             email,
+//           },
+//         });
+
+//         if (!user) {
+//           throw new Error('Invalid credentials');
+//         }
+
+//         const validPassword = await compareHashedPassword(
+//           password,
+//           user.password
+//         );
+
+//         if (!validPassword) {
+//           throw new Error('Invalid credentials');
+//         }
+
+//         return user;
+//       },
+//     }),
+//   ],
+//   pages: {
+//     signIn: '/login',
+//   },
+//   secret: process.env.JWR_SECRETE,
+//   cookies: {
+//     sessionToken: {
+//       name: process.env.COOKIES_NAME,
+//       options: {
+//         httpOnly: true,
+//         path: '/',
+//         sameSite: 'strict',
+//       },
+//     },
+//   },
+//   callbacks: {
+//     jwt({ token, user }) {
+//       if (user) {
+//         token.role = user.role;
+//         token.firstName = user.firstName;
+//         token.lastName = user.lastName;
+//         token.id = user.id;
+//         token.hasUploadedDocuments = user.hasUploadedDocuments;
+//         token.isCompanyVerified = user.isCompanyVerified;
+//         token.isEmailVerified = user.isEmailVerified;
+//       }
+
+//       return token;
+//     },
+
+//     session({ session, token }) {
+//       session.user.role = token?.role;
+//       session.user.firstName = token.firstName;
+//       session.user.lastName = token.lastName;
+//       session.user.id = token.id;
+//       session.user.hasUploadedDocuments = token.hasUploadedDocuments
+//         ? '1'
+//         : '0';
+//       session.user.isCompanyVerified = token.isCompanyVerified ? '1' : '0';
+//       session.user.isEmailVerified = token.isEmailVerified ? '1' : '0';
+
+//       return session;
+//     },
+//   },
+// });
+
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/db/postgresql';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compareHashedPassword } from '@/utils/password';
-import { jwtVerify, SignJWT } from 'jose';
-import { createToken } from '@/utils/jwtToken';
 
-export default NextAuth({
+const createOptions = (req) => ({
   session: {
     strategy: 'jwt',
     maxAge: 7 * 24 * 60 * 60,
@@ -76,7 +162,6 @@ export default NextAuth({
       session.user.lastName = token.lastName;
       session.user.id = token.id;
       session.user.hasUploadedDocuments = token.hasUploadedDocuments;
-
       session.user.isCompanyVerified = token.isCompanyVerified;
       session.user.isEmailVerified = token.isEmailVerified;
 
@@ -84,3 +169,11 @@ export default NextAuth({
     },
   },
 });
+
+const handler = async (req, res) => {
+  return NextAuth(req, res, createOptions(req));
+};
+
+export default handler;
+
+// export default NextAuth();
